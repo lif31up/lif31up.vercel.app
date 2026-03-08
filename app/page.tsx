bf83222f1791abecd8943511b59c3412b5fa2c25
ﾚﾉｷ乇1ᑌᑭ <nulranlcns9991@gmail.com>
2026-03-08 16:50:39 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -19,12 +19,12 @@ export default function Home() {
         <section className="xl:px-80 px-4 mt-8 lg:bg-transparent lg:border-0 bg-black border-t-2 border-neutral-800">
           <Projects />
           <GithubRepos />
+            <section className="lg:w-full h-fit flex items-center justify-center py-4 lg:mt-16 mt-4 gap-2 lg:text-xs text-xs text-neutral-500">
+                <h1>Laboratory Website – @lif31up powered by 2025</h1>
+                <a href="https://vercel.com/">▼ Vercel</a>
+            </section>
         </section>
       </RecoilRoot>
-      <section className="lg:w-full h-fit flex items-center justify-center py-4 lg:mt-16 mt-4 gap-2 lg:text-xs text-xs text-neutral-500">
-        <h1>Laboratory Website – @lif31up powered by 2025</h1>
-        <a href="https://vercel.com/">▼ Vercel</a>
-      </section>
     </section>
   );
 }
